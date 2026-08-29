276e2cf95aa2f3b0c1bc516fa80b6cc5b2340a68
emilio_sevilla_ortego <emiliosevilla@users.noreply.github.com>
2026-08-29 13:22:02 +0200

    2026-08-29_13-22-00

--- fuente/infrastructure/migrations/019_refinement_baseline_cas.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE refinement_candidates ADD COLUMN baseline_revision INTEGER NOT NULL DEFAULT 0;
-ALTER TABLE refinement_candidates ADD COLUMN baseline_content_hash TEXT NOT NULL DEFAULT '';

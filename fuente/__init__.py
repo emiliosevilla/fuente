26aa23d8c67dd09376e52749596e414554ee6acb
emilio_sevilla_ortego <emiliosevilla@users.noreply.github.com>
2026-09-01 19:49:01 +0200

    fix: trust bundled certificates for HTTPS (#112)

--- fuente/__init__.py
+++ fuente/__init__.py
@@ -1,2 +1,9 @@
 """Fuente Knowledge Base ETL Package."""
-__version__ = "0.2.8"
+import os
+
+from requests.certs import where
+
+
+os.environ.setdefault("SSL_CERT_FILE", where())
+
+__version__ = "0.2.9"
